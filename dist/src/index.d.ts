d2c2f41bf02192810d15d8b40bad9236bd944f61
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-07-11 14:27:51 +0800

    Untrack files in .gitignore

--- dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from "./lib/Request";
-declare function makeRequest(url: string | URL): Request;
-export { Request };
-export declare const request: (url: string | URL) => Request;
-export default makeRequest;
