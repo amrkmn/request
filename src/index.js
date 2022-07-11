44a42bc0ec57b3817b21868cfb1597ca7c2c044b
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-07-11 13:15:44 +0800

    1.0.4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const Request = require("../lib/Request");
 const makeRequest = (url) => new Request(url);
 
 module.exports = makeRequest;
-module.exports.Request = Request;
-module.exports.default = makeRequest;
-module.exports.request = makeRequest;
+module.exports = {
+    Request,
+    default: makeRequest,
+    request: makeRequest,
+};
